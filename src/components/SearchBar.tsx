fed8b80cb7b01e0ac32ad7c8797a56307543acc1
Manu Aguilera <Miguel>
2024-04-22 20:10:33 -0600

    Remove Filter Listeners

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -10,8 +10,8 @@ function SearchBar ({ filters, setFilters }: { filters: { search: string, listen
       <label htmlFor="search">Search: </label>
       <input type="text" name="search" value={filters.search} onChange={(e) => { setFilters(prevFilters => { return { ...prevFilters, search: e.target.value } }) }} />
       <label htmlFor="listeners">Listeners: </label>
-      <input type="range" min={1000} max={100000000} name="listeners" value={filters.listeners} onChange={(e) => { setFilters(prevFilters => { return { ...prevFilters, listeners: parseInt(e.target.value) } }) }} />
-      <span>{filters.listeners}</span>
+      {/* <input type="range" min={1000} max={100000000} name="listeners" value={filters.listeners} onChange={(e) => { setFilters(prevFilters => { return { ...prevFilters, listeners: parseInt(e.target.value) } }) }} />
+      <span>{filters.listeners}</span> */}
     </section>
   )
 }
